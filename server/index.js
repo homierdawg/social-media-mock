dd3261fbe6bf119d9d21c9732f4a3667350889ae
homierdawg <doggydude02@gmail.com>
2024-01-08 23:42:02 -0500

    index.js updated with user and posts imports

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ import postRoutes from "./routes/posts.js";
 import { register} from "./Controllers/auth.js";
 import { createPost } from "./Controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/user.js";
+import Post from "./models/uost.js";
+import { users, posts } from "./data/index.js";
 
 
 /* configurations - rdawg */
@@ -62,4 +65,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT} `));
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`)); 
